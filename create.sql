45413797f1a73c6cddef593000297b31f08c420c
Balázs Bartha <balifej@gmail.com>
2020-05-26 23:56:36 +0200

    added checkstyle.xml

--- create.sql
+++ create.sql
@@ -138,3 +138,13 @@ create sequence hibernate_sequence start with 1 increment by 1
         solved boolean not null,
         primary key (id)
     )
+create sequence hibernate_sequence start with 1 increment by 1
+
+    create table Result (
+       id bigint not null,
+        created timestamp not null,
+        duration bigint not null,
+        playerName varchar(255) not null,
+        solved boolean not null,
+        primary key (id)
+    )
